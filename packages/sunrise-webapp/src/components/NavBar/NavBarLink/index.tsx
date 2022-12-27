270ef78b38e40db243fa89aedb4ba439cf73e673
Lauren Dorman <hello@laurendorman.io>
2022-12-27 21:49:52 +0100

    chore: update package versions

--- packages/sunrise-webapp/src/components/NavBar/NavBarLink/index.tsx
+++ packages/sunrise-webapp/src/components/NavBar/NavBarLink/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import classes from './NavBarLink.module.css';
-
 const NavBarLink: React.FC<{ to: string; className?: string }> = props => {
   const { to, children, className } = props;
   // hacks to make React Router work with external links
@@ -13,6 +12,7 @@ const NavBarLink: React.FC<{ to: string; className?: string }> = props => {
       onClick={onClick}
       target={target}
     >
+      {/* @ts-ignore */}
       {children}
     </Link>
   );
